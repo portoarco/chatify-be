491cd6b77c05771a39f92eb022ac6b6007e240c1
portoarco <arcoanggoro78@gmail.com>
2025-10-26 19:09:40 +0700

    Update bug deployment

--- dist/src/configs/prisma.d.ts
+++ dist/src/configs/prisma.d.ts
@@ -1,3 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-export declare const prisma: PrismaClient<import(".prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
+export declare const prisma: PrismaClient<{
+    log: ("error" | "info" | "query" | "warn")[];
+}, "error" | "info" | "query" | "warn", import("@prisma/client/runtime/library").DefaultArgs>;
 //# sourceMappingURL=prisma.d.ts.map
